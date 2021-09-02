0a4feb09c26daee66447dee00681bbd4ad269a98
JJLee9527 <dgava25@gmail.com>
2021-09-02 14:42:39 +0800

    dist folder added

---
+++ dist/_nuxt/9c4355b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{296:function(n,e,t){"use strict";t.r(e);var l=t(13),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("很抱歉，您不是全聯的正職員工，無法參與當團爸團媽")])}),[],!1,null,null,null);e.default=component.exports}}]);
